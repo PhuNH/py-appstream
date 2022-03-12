6c6b63ae7fa29f6343f0e3eeff3e84c2cd87ff72
PhuNH <1079742+PhuNH@users.noreply.github.com>
2022-03-12 12:39:15 +0100

    Stop escaping quotes

--- py_appstream/utils.py
+++ py_appstream/utils.py
@@ -7,7 +7,7 @@ import html
 def localize(out_obj, node, f=None):
     lang = node.get('{http://www.w3.org/XML/1998/namespace}lang', node.get('lang', 'C'))
     if lang != 'x-test':
-        out_obj[lang] = f(node.text) if f else html.escape(node.text.strip())
+        out_obj[lang] = f(node.text) if f else html.escape(node.text.strip(), quote=False)
 
 
 def join_lines(txt: str):
