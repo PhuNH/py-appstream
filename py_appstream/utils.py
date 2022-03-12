f548739c1f041cc8ee8b3862b60424719a5d4a6b
PhuNH <1079742+PhuNH@users.noreply.github.com>
2022-03-12 13:15:13 +0100

    Stop HTML escaping for texts other than descriptions
    They are not to be used directly as HTML, so there's no need to escape them by ourselves

--- py_appstream/utils.py
+++ py_appstream/utils.py
@@ -1,13 +1,10 @@
 # SPDX-FileCopyrightText: 2022 Phu Hung Nguyen <phuhnguyen@outlook.com>
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-import html
-
-
 def localize(out_obj, node, f=None):
     lang = node.get('{http://www.w3.org/XML/1998/namespace}lang', node.get('lang', 'C'))
     if lang != 'x-test':
-        out_obj[lang] = f(node.text) if f else html.escape(node.text.strip(), quote=False)
+        out_obj[lang] = f(node.text) if f else node.text.strip()
 
 
 def join_lines(txt: str):
