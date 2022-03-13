774abf8a9b59d7da8d029671135086fecea49595
PhuNH <1079742+PhuNH@users.noreply.github.com>
2022-03-13 17:24:16 +0100

    Handle screenshot captions containing XML elements

--- py_appstream/utils.py
+++ py_appstream/utils.py
@@ -1,10 +1,13 @@
 # SPDX-FileCopyrightText: 2022 Phu Hung Nguyen <phuhnguyen@outlook.com>
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+from xml.etree import ElementTree
+
+
 def localize(out_obj, node, f=None):
     lang = node.get('{http://www.w3.org/XML/1998/namespace}lang', node.get('lang', 'C'))
     if lang != 'x-test':
-        val = node.text.strip()
+        val = ElementTree.tostring(node, encoding='unicode', method='text').strip()
         out_obj[lang] = f(val) if f else val
 
 
