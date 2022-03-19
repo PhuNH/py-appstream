2abe3eb1e3cd5617509da431d512633814935081
PhuNH <1079742+PhuNH@users.noreply.github.com>
2022-03-19 17:33:13 +0100

    Support customizing language codes

--- py_appstream/utils.py
+++ py_appstream/utils.py
@@ -4,11 +4,12 @@
 from xml.etree import ElementTree
 
 
-def localize(out_obj, node, f=None):
+def localize(out_obj, node, f=None, lang_code_func=None):
     lang = node.get('{http://www.w3.org/XML/1998/namespace}lang', node.get('lang', 'C'))
     if lang != 'x-test':
         val = ElementTree.tostring(node, encoding='unicode', method='text').strip()
         if val:
+            lang = lang_code_func(lang) if lang_code_func else lang
             out_obj[lang] = f(val) if f else val
 
 
